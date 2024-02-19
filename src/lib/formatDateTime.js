877b244289b697f38a48eb89e5fc7350899b0e2f
MoazamAli45 <21-se-48@students.uettaxila.edu.pk>
2024-02-19 23:22:20 +0500

    Remove axios dependency and update date formatting

--- src/lib/formatDateTime.js
+++ src/lib/formatDateTime.js
@@ -1,32 +1,11 @@
 export function formatDateTime(dateTimeString) {
   const date = new Date(dateTimeString);
-  // Create a new Date object
-  console.log(date, "date");
-  // Get year, month, day, hours, and minutes
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1; // Months are zero-based
-  const day = date.getDate();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
 
-  // Convert hours to 12-hour format
-  const formattedHours = hours % 12;
-  console.log(formattedHours, "formattedHours");
-  // Determine AM or PM
-  const period = hours < 12 ? "AM" : "PM";
+  const options = {
+    timeZone: "UTC", // Set the time zone to UTC or the appropriate time zone
+    // other options as needed
+  };
+  console.log(date.toLocaleString("en-US", options)); // Adjust locale as needed
 
-  // Construct the final date string
-  const formattedDate = `${year}-${month < 10 ? "0" : ""}${month}-${
-    day < 10 ? "0" : ""
-  }${day}`;
-
-  // Construct the final time string
-  const formattedTime = `${formattedHours}:${
-    minutes < 10 ? "0" : ""
-  }${minutes} ${period}`;
-
-  // Concatenate date and time
-  const formattedDateTime = `${formattedDate} ${formattedTime}`;
-
-  return formattedDateTime;
+  return date.toLocaleString("en-NZ", options);
 }
